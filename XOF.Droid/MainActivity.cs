19a136e69c37ff55bb51e03a47a191e004507cf5
William S Rodriguez <williamsrz@outlook.com>
2016-09-29 16:23:38 -0300

    Add project files.

---
+++ XOF.Droid/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace XOF.Droid
+{
+    [Activity(Label = "XOF.Droid", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/AppTheme")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.ActivityMain);
+        }
+    }
+}
+
